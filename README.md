475c51fc19a3e61bc9edcb2ad94bbef738f2df98
Lab Rat <dev.frolics@gmail.com>
2024-03-15 00:57:07 +0530

    feat: foss weekened init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Jumble Words
+
+Jumble Words is a single player game app made using kotlin that displays scrambled words. To play the game, player has to make a word using all the letters from the displayed scrambled word.
+
+## Setup Instructions
+
+Follow these steps to run the Jumble Words on your local machine:
+
+### Prerequisites
+
+- Make sure you have Android Studio on your system.
+- Ensure you have an emulator or a physical device configured for running android app.
+
+### Installation
+
+1. **Fork** and **Clone** the repository to your local machine:
+
+```bash
+git clone https://github.com/iiitl/Jumble-Words
+```
+
+### Running the App
+
+1. Run android studio and open the project.
+
+1. Ensure your emulator is installed on android studio.
+
+2. Run the app 
+
+
+This will build and install the Jumble word app on your connected device or emulator.
+
+### Preview
+
+<h1 align="center">
+  <img width="270" height="500" src="https://github.com/JustArepo/Jumble-Words/assets/163036582/bd5250f6-2ab4-4c17-88a2-2890e5b324c1">
+  <img width="270" height="500" src="https://github.com/JustArepo/Jumble-Words/assets/163036582/9bb4fffb-c296-4a19-98de-13e087183cc1">
+</h1>
+
+### Additional Configuration
+
+- If you encounter any issues during setup or while running the app, reach out to the project maintainers for assistance.
+- Explore the project structure and source code to understand how the Jumble Word app works and how you can contribute to its development.
+
+## Contributing
+
+Contributions to the project are welcome!. Additionally, pull requests are highly appreciated.
---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private var _score = MutableLiveData(0)
+    val score: LiveData<Int>
+        get() = _score
+
+    private var _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
+
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String>
+        get() = _currentScrambledWord
+
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord.value = currentWord
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
+            wordsList.add(currentWord)
+        }
+
+    }
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    private fun increaseScore() {
+        _score.value = 0
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+}
